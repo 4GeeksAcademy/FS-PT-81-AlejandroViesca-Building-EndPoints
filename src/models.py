1b141be994e7d76e936262c5f331f0bc67462c9e
ViesK <109549664+ViesK@users.noreply.github.com>
2025-02-13 19:11:03 +0000

    db

--- src/models.py
+++ src/models.py
@@ -15,5 +15,59 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
+class Persons(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    favourite_of = db.relationship('Favourite_persons', back_populates='person_relationship')
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "planet_id": self.planet_id,
+            "favourite_of": [person.serialize() for person in self.favourite_of] if self.favourite_of else None
+        }
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    persons = db.relationship('Persons', backref=('planet'))
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "persons": [person.serialize() for person in self.persons] if self.persons else None
+        }   
+
+
+class Favourite_persons(db.Model):
+    __tablename__ = 'favourite_persons'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_relationship = db.relationship('Users', back_populates='person_favourites')
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+    person_relationship = db.relationship('Persons', back_populates='favourite_of')
+
+    def __repr__(self):
+         return '<Favourite_persons %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id" : self.user_id,
+            "person_id" : self.person_id
+    }
